417626a6bde22096a15b6e5b671dc8c8c56b6da3
Jiangshan00001 <710806594@qq.com>
2022-01-09 14:47:23 +0800

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include "NFAState.h"
 #include "ORegexParse.h"
+#include "nfaconvert.h"
 /// 堆栈方法求值
 /// http://blog.csdn.net/liuhuiyi/article/details/8433203
 
@@ -14,16 +15,19 @@
 ///
 int main()
 {
-	ORegexParse mRegex;
-	
-	mRegex.CreateNFA("abca*");
-	
-	mRegex.NFAToDFA();
+    ORegexParse mRegex;
+    NFAConvert mConvert;
+
+    //FSA_TABLE nfa = mRegex.CreateNFA("abca*");
+    FSA_TABLE nfa = mRegex.CreateNFA("(a|b)*(aa|bb)*");
+    FSA_TABLE dfa = mConvert.NFAtoDFA(nfa);
+    fsa_to_dot(nfa, "nfa1.dot");
+    fsa_to_dot(dfa, "dfa1.dot");
 
 
 
 
-	return 0;
+    return 0;
 }
 
 #endif
